85a4000c899bab33f7ee27aee35c4fbe3ecdfe09
riyashah17 <rhshah171198@gmail.com>
2018-07-02 00:24:06 +0530

    Using Observables

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -2,27 +2,26 @@ import { Injectable } from '@angular/core';
 import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
 
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/observable/of';
+
 @Injectable()
 export class LeaderService {
 
   constructor() { }
 
   getLeaders(): Promise<Leader[]>{
-  	return new Promise(resolve=>{
-      setTimeout(() => resolve(LEADERS),2000);
-  });
+    return Observable.of(LEADERS).delay(2000).toPromise();
   }
 
-  getLeader(id: number) : Promise<Leader>{
-  	return new Promise(resolve=>{
-      setTimeout(() => resolve(LEADERS.filter((leader) => (leader.id === id))[0]),2000);
-  });
+  getLeader(id:number) : Promise<Leader>{
+    return Observable.of(LEADERS.filter((leader) => (leader.id === id))[0]).delay(2000).toPromise();
   }
 
   getFeaturedLeader(): Promise<Leader>{
-  	return new Promise(resolve=>{
-      setTimeout(() => resolve(LEADERS.filter((leader) => leader.featured)[0]),2000);
-  });
+    return Observable.of(LEADERS.filter((leader) => leader.featured)[0]).delay(2000).toPromise();
   }
 
 }
