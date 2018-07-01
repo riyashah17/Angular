43e7db0ac971681d19c9de867e7dd62712717da4
riyashah17 <rhshah171198@gmail.com>
2018-07-02 00:32:23 +0530

    Using observables in place of promises

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -3,7 +3,6 @@ import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
 
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/delay';
 import 'rxjs/add/observable/of';
 
@@ -12,16 +11,16 @@ export class LeaderService {
 
   constructor() { }
 
-  getLeaders(): Promise<Leader[]>{
-    return Observable.of(LEADERS).delay(2000).toPromise();
+  getLeaders(): Observable<Leader[]>{
+    return Observable.of(LEADERS).delay(2000);
   }
 
-  getLeader(id:number) : Promise<Leader>{
-    return Observable.of(LEADERS.filter((leader) => (leader.id === id))[0]).delay(2000).toPromise();
+  getLeader(id:number) : Observable<Leader>{
+    return Observable.of(LEADERS.filter((leader) => (leader.id === id))[0]).delay(2000);
   }
 
-  getFeaturedLeader(): Promise<Leader>{
-    return Observable.of(LEADERS.filter((leader) => leader.featured)[0]).delay(2000).toPromise();
+  getFeaturedLeader(): Observable<Leader>{
+    return Observable.of(LEADERS.filter((leader) => leader.featured)[0]).delay(2000);
   }
 
 }
