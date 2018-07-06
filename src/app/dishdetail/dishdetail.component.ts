bbde074bd94322bb5d72e89dd7103b0087c08919
riyashah17 <rhshah171198@gmail.com>
2018-07-06 16:42:48 +0530

    Adding flyInOut animation

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -6,26 +6,22 @@ import { DishService } from '../services/dish.service';
 import 'rxjs/add/operator/switchMap';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Comment } from '../shared/comment';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { visibility, flyInOut } from '../animations/app.animation';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+  },
   animations: [
-    trigger( 'visibility',[
-      state('shown', style({
-        transform: 'scale(1.0)',
-        opacity: 1
-      })),
-      state('hidden', style({
-        transform: 'scale(0.5)',
-        opacity: 0
-      })),
-      transition('* => *',animate('0.5s ease-in-out'))
-    ])
+    visibility(),
+    flyInOut()
   ]
 })
+
 export class DishdetailComponent implements OnInit {
 
   dish: Dish;
